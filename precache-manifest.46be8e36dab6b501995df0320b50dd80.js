66ebe65c4d7a3d74f2fc9bb5cfa6a569546361aa
Abner <sbner2@gmail.com>
2020-06-17 20:15:23 -0300

    Updates

---
+++ precache-manifest.46be8e36dab6b501995df0320b50dd80.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c3d36a2fc8fe69aa2187d71be97a03",
+    "url": "/fetchPokemon/index.html"
+  },
+  {
+    "revision": "ad8d15e3143e9b6a5108",
+    "url": "/fetchPokemon/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "854566d5ae973298059a",
+    "url": "/fetchPokemon/static/css/main.2d83c858.chunk.css"
+  },
+  {
+    "revision": "ad8d15e3143e9b6a5108",
+    "url": "/fetchPokemon/static/js/2.8d853b2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fetchPokemon/static/js/2.8d853b2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "854566d5ae973298059a",
+    "url": "/fetchPokemon/static/js/main.0549bbd1.chunk.js"
+  },
+  {
+    "revision": "f8db25a9f412e614ba28",
+    "url": "/fetchPokemon/static/js/runtime-main.e9b54e0f.js"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/fetchPokemon/static/media/Pokemon Solid.36d26dbc.ttf"
+  },
+  {
+    "revision": "6029a9b400de800ab6cf52cce916c219",
+    "url": "/fetchPokemon/static/media/pokemon-bg.6029a9b4.png"
+  },
+  {
+    "revision": "78745eb1af18d9a1902208f513b89487",
+    "url": "/fetchPokemon/static/media/whosthatpokemon.78745eb1.png"
+  }
+]);
